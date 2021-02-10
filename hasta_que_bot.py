68bfc27cf986da517a5375a37cc660ba045f8e06
pperone <pperone@gmail.com>
2021-02-10 13:57:00 -0300

    Testing authentication and query

--- hasta_que_bot.py
+++ hasta_que_bot.py
@@ -1,15 +1,10 @@
 import tweepy
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler("pGBDoAaEpkliVKBOLwjtcmHGc", 
-    "xF3g1wrP50b6BlZEd20u4oVfjgH1FGQcuWUzlQO5aUWOufvlhw")
-auth.set_access_token("622518493-6VcLIPprbQbv9wkcBBPvCle8vsjU9fE85Dq9oStl", 
-    "tH9aKQbQQ1iRdYTcLSsPwitl44BkAc6jilrsU0ifnXvZhq")
+# OAuth2 authentication
+auth = tweepy.AppAuthHandler("fwBcUpUv0O193Pe5O33qBlBLq", 
+    "XRDPn08Zd0ARDRIkqjn7x9T2A8lSMmXF31khySWVRRLF8141T3")
 
 api = tweepy.API(auth)
 
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
+for tweet in tweepy.Cursor(api.search, q='tweepy').items(10):
+    print(tweet.text)'
